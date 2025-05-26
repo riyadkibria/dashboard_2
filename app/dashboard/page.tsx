5635e48e45b41cbf4a04375fde1bf7505c0b6c64
riyadkibria <riyad.kibria@gmail.com>
2025-05-26 22:31:31 +0600

    page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+// app/dashboard/page.tsx
+export default function DashboardHome() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Dashboard Overview</h1>
+    </div>
+  );
+}
