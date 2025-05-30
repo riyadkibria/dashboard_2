2788f68d63ff2a75923d421cf03a7d84ec6885a7
riyadkibria <riyad.kibria@gmail.com>
2025-05-30 23:37:06 +0600

    layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,9 @@
 // app/dashboard/layout.tsx
 "use client";
+import { useState } from "react";
 import Sidebar from "@/components/Sidebar";
 import Topbar from "@/components/Topbar";
-import { useState } from "react";
+
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   const [isCollapsed, setIsCollapsed] = useState(false);
