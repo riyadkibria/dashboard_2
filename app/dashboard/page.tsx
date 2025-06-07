50d5b98362ed59f1be7418381eb420ceba85300a
riyadkibria <riyad.kibria@gmail.com>
2025-06-08 00:49:19 +0600

    page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 // lib/fetchAllOrders.ts
 import { collection, getDocs, Timestamp } from "firebase/firestore";
-import { db } from "./firebase";
+import { db } from "../../firebase";
 
 interface Order {
   orderId: string;
