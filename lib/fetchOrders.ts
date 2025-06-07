37c072518154d922c2b3ae2795b1effef41e2cdd
riyadkibria <riyad.kibria@gmail.com>
2025-06-08 01:54:27 +0600

    fetchOrders.ts

--- lib/fetchOrders.ts
+++ lib/fetchOrders.ts
@@ -1,1 +1,28 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
+
+export async function fetchAllOrders() {
+  try {
+    const usersSnapshot = await getDocs(collection(db, "users"));
+    const allOrders = [];
+
+    for (const userDoc of usersSnapshot.docs) {
+      const userId = userDoc.id;
+      const ordersSnapshot = await getDocs(collection(db, `users/${userId}/orders`));
+      
+      ordersSnapshot.forEach(orderDoc => {
+        allOrders.push({
+          userId,
+          orderId: orderDoc.id,
+          ...orderDoc.data(),
+        });
+      });
+    }
+
+    return allOrders;
+  } catch (error) {
+    console.error("Error fetching all orders:", error);
+    return [];
+  }
+}
 
