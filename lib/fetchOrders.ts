6226af5d70c16f850f27cea0ab1b97b30f577b1e
riyadkibria <riyad.kibria@gmail.com>
2025-06-08 03:21:38 +0000

    fetchOrders.ts

--- lib/fetchOrders.ts
+++ lib/fetchOrders.ts
@@ -1,1 +1,25 @@
+// lib/fetchOrders.ts
+import { collectionGroup, getDocs, Timestamp } from "firebase/firestore";
+import { db } from "./firebase";
+
+export type Order = {
+  address: string;
+  createdAt: Timestamp;
+  mobile: string;
+  name: string;
+  productName: string;
+  productPrice: string;
+};
+
+export const fetchOrders = async (): Promise<Order[]> => {
+  const snapshot = await getDocs(collectionGroup(db, "orders"));
+  const orders: Order[] = [];
+
+  snapshot.forEach((doc) => {
+    const data = doc.data() as Order;
+    orders.push(data);
+  });
+
+  return orders;
+};
 
