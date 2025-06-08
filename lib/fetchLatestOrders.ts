36258e2034f59bb106c7b5104ed1146588451264
riyadkibria <riyad.kibria@gmail.com>
2025-06-08 11:53:52 +0600

    fetchLatestOrders.ts

--- lib/fetchLatestOrders.ts
+++ lib/fetchLatestOrders.ts
@@ -1,5 +1,5 @@
 import { collection, getDocs, Timestamp } from "firebase/firestore";
-import { db } from "../lib/firebase";
+import { db } from "./firebase";
 
 export type OrderData = {
   userId: string;
