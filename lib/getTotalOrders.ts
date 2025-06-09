514bb6769714d054feb35426211f8bd2454c9a50
riyadkibria <riyad.kibria@gmail.com>
2025-06-09 10:59:20 +0600

    getTotalOrders.ts

--- lib/getTotalOrders.ts
+++ lib/getTotalOrders.ts
@@ -1,6 +1,7 @@
 import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 export async function getTotalOrders(): Promise<number> {
-  const snapshot = await db.collection("orders").get();
+  const snapshot = await getDocs(collection(db, "user_request")); // use your actual collection name
   return snapshot.size;
 }
