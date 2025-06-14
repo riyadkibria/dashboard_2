e49a1f7a39c1460352e50871fa97613724923204
riyadkibria <riyad.kibria@gmail.com>
2025-06-14 20:47:28 +0600

    latestorders.tsx

--- lib/latestorders.tsx
+++ lib/latestorders.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { getLatestOrders, UserRequest } from "@/lib/latestOrders";
+import { getLatestOrders, UserRequest } from "@/lib/latestorders";
 import {
   UserIcon,
   PhoneIcon,
