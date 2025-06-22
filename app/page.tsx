265dce74c213ac2795ee303b6ab07adee800594a
riyadkibria <riyad.kibria@gmail.com>
2025-06-22 22:39:35 +0600

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <div className="home-page">
       <h1 className="welcome-text">Welcome</h1>
+      <div className="welcome-subtext">Glad to see you here.</div>
     </div>
   );
 }
+
